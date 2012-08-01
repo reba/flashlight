704ecc5d6bdbd4ebc594f24fcb51dfe6ce24cc57
Claudio Marcelo Basckeira <ra042674@cervantes.lab.ic.unicamp.br>
2012-08-01 19:01:49 -0300

    v0.0.0 with pure HTML+CSS+jquery UI. Not tested on actual b2g yet.

---
+++ js/flashlight.js
@@ -0,0 +1,15 @@
+//Function to convert hex format to a rgb color
+function rgb2hex(r,g,b){
+ return "#" +
+  ("0" + parseInt(r,10).toString(16)).slice(-2) +
+  ("0" + parseInt(g,10).toString(16)).slice(-2) +
+  ("0" + parseInt(b,10).toString(16)).slice(-2);
+}
+
+$(document).ready(function(){
+	$( "#slider" ).slider({
+		slide: function(event,ui){
+			$('body').css("background-color",rgb2hex(ui.value*2.5,ui.value*2.5,ui.value*2.5));
+		}
+	});
+});
