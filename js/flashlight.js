1a02462953ec2592a6b9c103e6ed49725d8a6a3e
Claudio Marcelo Basckeira <dbahamut@gmail.com>
2012-08-03 15:32:45 -0300

    Signed-off-by: Claudio Marcelo Basckeira <dbahamut@gmail.com>

--- js/flashlight.js
+++ js/flashlight.js
@@ -1,3 +1,30 @@
+function verifyInstallation(){
+	var request = navigator.mozApps.getSelf();
+	request.onsuccess = function() {
+	  if (request.result) {
+		// we're installed
+		$(installButton).hide();
+	  } else {
+		// not installed
+	  }
+	}
+	request.onerror = function() {
+	  alert('Error checking installation status: ' + this.error.message);
+	}
+}
+
+function requestInstallation(){
+	var request = navigator.mozApps.install("../manifest.json");
+	request.onsuccess = function() {
+	  // great - display a message, or redirect to a launch page
+	  window.reload();
+	}
+	request.onerror = function() {
+	  // whoops - this.error.name has details
+	  alert('Error installing: ' + this.error.name);
+	}
+}
+
 //Function to convert a rgb color to hex format
 function rgb2hex(r,g,b){
  return "#" +
@@ -7,6 +34,7 @@ function rgb2hex(r,g,b){
 }
 
 $(document).ready(function(){
+	verifyInstallation();
 	//Transforming the #slider div in an actual jquery UI slider.
 	$( "#slider" ).slider({
 		//Whenever the slider slides, change the background color.
